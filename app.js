7d3fd2aceb63cc2ff41fde553eb97d8194448a9e
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-05-10 22:30:30 +0200

    optimized data for vagrant

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.static('public'));
 // otherwise an error is thrown
 app.set('view engine', 'jade');
 
-baseUrl = "http://localhost:3001";
+//baseUrl = "http://localhost:3001";
+baseUrl = "http://13.13.13.15:3001";
 
 var server = require('http').Server(express);
 io = require('socket.io')(server);
