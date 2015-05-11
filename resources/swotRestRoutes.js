25d890da595296b2e5bc7e50e92232d904614ef0
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-05-11 19:43:47 +0200

    Update swotRestRoutes.js

--- resources/swotRestRoutes.js
+++ resources/swotRestRoutes.js
@@ -1,9 +1,9 @@
 var swotRestRoutes = {
     "routes": {
-        "post_messages": "http://13.13.13.13/app.php/api/v1/thing/messages",
-        "post_functions_update": "http://13.13.13.13/app.php/api/v1/thing/functions/update",
-        "post_info_update": "http://13.13.13.13/app.php/api/v1/thing/information/update",
-        "post_image_update" : "http://13.13.13.13/app.php/api/v1/thing/profileimage/update"
+        "post_messages": "http://13.13.13.13/api/v1/thing/messages",
+        "post_functions_update": "http://13.13.13.13/api/v1/thing/functions/update",
+        "post_info_update": "http://13.13.13.13/api/v1/thing/information/update",
+        "post_image_update" : "http://13.13.13.13/api/v1/thing/profileimage/update"
     }
 };
 
